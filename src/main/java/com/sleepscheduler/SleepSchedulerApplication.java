f02064e488f7ccd8cc5a2aa0e243ac6511b24bf4
KhushiGupta2004 <khushigup2023@gmail.com>
2026-04-18 10:21:36 +0530

    Add files via upload

---
+++ src/main/java/com/sleepscheduler/SleepSchedulerApplication.java
@@ -0,0 +1,12 @@
+package com.sleepscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleepSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SleepSchedulerApplication.class, args);
+    }
+}
